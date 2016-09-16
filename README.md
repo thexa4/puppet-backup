3bb527fe5deb720b219c1e99a86529d7efa1f4cb
Max Maton <max@maton.info>
2016-09-16 09:04:55 +0000

    Updated version

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 #Backup
 This configures backups using duplicity and [SecretServer](https://github.com/thexa4/secrets-server).
 
-Uses [PKI module](https://github.com/thexa4/puppet-pki) to automatically setup client certificates.
+Uses [PKI module](https://forge.puppet.com/thexa4/pki) to automatically setup client certificates.
 
 ##Usage
 Available parameters:
 
 * `$secret_host`
+* `$backup_destination`
 * `$ca = "/etc/ssl/certs/host-ca.crt"`
 * `$cert = "/etc/ssl/certs/host.crt"`
 * `$key = "/etc/ssl/private/host.key"`
+* `$excludes = ['/proc','/sys','/dev','/proc','/run','/media','/mnt','/pub','**/.cache']`
 
+##See also
+Works with [backup_hubic](https://forge.puppet.com/thexa4/backup_hubic) if you need Hubic as a target
--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,17 @@ class backup(
   $ca = '/etc/ssl/certs/host-ca.crt',
   $cert = '/etc/ssl/certs/host.crt',
   $key = '/etc/ssl/private/host.key',
+  $excludes = [
+    '/proc',
+    '/sys',
+    '/dev',
+    '/proc',
+    '/run',
+    '/media',
+    '/mnt',
+    '/pub',
+    '**/.cache',
+  ],
 ) {
 
   package { 'duply':
@@ -23,12 +34,23 @@ class backup(
     require => File['/etc/duply'],
   }
 
+  file { '/etc/duply/default/exclude':
+    ensure  => file,
+    content => join($excludes, "\n"),
+    require => File['/etc/duply/default'],
+  }
+
   file { '/etc/duply/default/pre.d':
     ensure  => directory,
     require => File['/etc/duply/default'],
   }
   
   file { '/etc/duply/default/post.d':
+    ensure  => directory,
+    require => File['/etc/duply/default'],
+  }
+  
+  file { '/etc/duply/default/conf.d':
     ensure  => directory,
     require => File['/etc/duply/default'],
   }
@@ -48,13 +70,14 @@ class backup(
       File[$cert],
       File[$key],
       File['/etc/duply/default/conf'],
+      File['/etc/duply/default/conf.d'],
       File['/etc/duply/default/pre.d'],
       File['/etc/duply/default/pre'],
       File['/etc/duply/default/post.d'],
       File['/etc/duply/default/post'],
       Package['wget'],
     ],
-    command => "/usr/bin/wget --ca-certificate=\"${ca}\" --certificate=\"${cert}\" --private-key=\"${key}\" https://${secret_host}/gpg --output-document=/etc/duply/default/gpgkey.${substituted_host}.asc && (/usr/bin/duply default status || true)",
+    command => "/usr/bin/wget --ca-certificate=\"${ca}\" --certificate=\"${cert}\" --private-key=\"${key}\" https://${secret_host}/gpg --output-document=/etc/duply/default/gpgkey.${substituted_host}.asc && (/usr/bin/duply default status || true) || (rm /etc/duply/default/gpgkey.${substituted_host}.asc && exit 1)",
     #logoutput => true,
   }
 
@@ -77,5 +100,23 @@ class backup(
     ensure  => file,
     content => "#!/bin/bash\n# Managed by puppet\nDIR=\"\$( cd \"\$( dirname \"\${BASH_SOURCE[0]}\" )\" && pwd )\"\nrun-parts \"\$DIR/post.d\"\n",
     require => File['/etc/duply/default'],
+  }
+
+  if (!defined(Package['anacron'])) {
+    package{ 'anacron':
+      ensure => installed,
+    }
+  }
+
+  file { '/etc/cron.daily/duplicity-backup':
+    ensure  => file,
+    mode    => '0775',
+    content => '#!/bin/bash\nduply default backup >> /var/log/backup.log',
+  }
+  
+  file { '/etc/cron.weekly/duplicity-backup-cleanup':
+    ensure  => file,
+    mode    => '0775',
+    content => '#!/bin/bash\nduply default purge >> /var/log/backup.log\nduply default purgeFull >> /var/log/backup.log\nduply default purgeIncr >> /var/log/backup.log',
   }
 }
